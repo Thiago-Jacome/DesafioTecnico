611326535734278751c5d803e30b48a6c538b32c
Thiago <thiago_jb94@hotmail.com>
2026-07-21 06:31:04 -0300

    cofigurando banco de dados.

---
+++ Database/MySqlConnection.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+namespace DesafioTecnico.Database
+{
+    public class MySqlConnection(IConfiguration configuration)
+    {
+        public MySqlConnection CreatConnection(string banco)
+        {
+            var connectionString = configuration.GetConnectionString(banco)
+            ?? throw new InvalidOperationException($"Connection string '{banco}' não configurada.");
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
